ec459b2dc352a7289fae39f99192a1aa93bb4e53
Nelde95 <Nelde95@yahoo.com>
2020-04-24 10:33:57 +0200

    updated scripts to match normalized database

--- dir_list.sh
+++ dir_list.sh
@@ -1,2 +1,2 @@
 sqlite3 files.db "SELECT f.filename FROM Fil AS f
-WHERE f.par = \""$1"\";"
+WHERE f.parid = \""$1"\";"
--- inode_path.sh
+++ inode_path.sh
@@ -5,9 +5,9 @@ sqlite3 files.db "SELECT COALESCE(NULLIF(
 	|| COALESCE(rtrim(f1.filename,\"/\"), \"\"), \"\"), \"/\")
 FROM Fil AS f1
 LEFT JOIN Fil AS f2
-ON f1.par = f2.id
+ON f1.parid = f2.id AND f1.pardevicenum = f2.devicenum
 LEFT JOIN Fil AS f3
-ON f2.par = f3.id
+ON f2.parid = f3.id AND f2.pardevicenum = f3.devicenum
 LEFT JOIN Fil AS f4
-ON f3.par = f4.id
+ON f3.parid = f4.id AND f3.pardevicenum = f4.devicenum
 WHERE f1.id = \""$1"\";"
